730c757e8844b4ed124fc7c402821f9867d33aef
Shawn Willard <shawnmwillard@gmail.com>
2022-05-27 16:52:02 -0500

    added cover image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <main>
+        <Footer></Footer>
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <section>
-      <h1 id="footer">Footer Text</h1>
+    <section className="my-5">
+      <h1 id="footer">Contact me via email or GitHub for more information</h1>
     </section>
   );
 }
